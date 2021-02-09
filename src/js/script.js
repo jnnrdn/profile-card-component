269eb52ef17f5d6332e11a3d7b1240f5e9e5b5f3
Jenny Ryden <jennyryden@protonmail.com>
2021-02-09 15:50:25 +0100

    Fix mobile landscape issues
    - No spacing above and below card
    - Footer overlapping content (grid application and perhaps mobile viewport issue)
    
    Helpful articles:
    - https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
    - https://css-tricks.com/preventing-a-grid-blowout/

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,4 @@
-function toggleMobile() {
-  var x = document.getElementById('myNav')
-  var icon = document.getElement
-  if (x.style.display === 'block') {
-    x.style.display = 'none'
-    x.classList.remove('open')
-  } else {
-    x.style.display = 'block'
-    x.classList.add('open')
-  }
-}
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
